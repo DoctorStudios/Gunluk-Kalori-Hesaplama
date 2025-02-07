9be0c163c79342539b06c381dd1d971bc0a7fcda
Aderfen <doctorstudiostr@gmail.com>
2025-02-07 12:16:40 +0300

    Proje dosyası ekle.

---
+++ Günlük Kalori Hesaplama/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Günlük_Kalori_Hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilo;
+            int yaş;
+            double boy;
+            int cinsiyet = 0;
+            double BMR = 0;
+            int hareket = 0;
+            double TDEE;
+            double seçim;
+            double sonuç;
+        AnaMenu:
+            Console.WriteLine("Bazal Metabolizma Hızını (BMR) Hesaplama");
+            Console.WriteLine("Lütfen cinsiyetinizi girin:");
+            Console.WriteLine("[1] Erkek:");
+            Console.WriteLine("[2] Kadın:");
+            cinsiyet = int.Parse(Console.ReadLine());
+            switch (cinsiyet)
+            {
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Lütfen Geçerli bir sayı giriniz;");
+                    goto AnaMenu;
+
+                case 1:
+                    Console.WriteLine("Lütfen Kilonuzu girin");
+                    kilo = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Lütfen Boyunuzu girin");
+                    boy = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Lütfen Yaşınızı girin");
+                    yaş = int.Parse(Console.ReadLine());
+                    BMR = (88.36 + (13.4 * kilo)) + (4.8 * boy) - (5.7 * yaş);
+                    Console.WriteLine($"Bazal Metabolizma Hızınız {BMR}'dır");
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Lütfen Kilonuzu girin");
+                    kilo = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Lütfen Boyunuzu girin");
+                    boy = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Lütfen Yaşınızı girin");
+                    yaş = int.Parse(Console.ReadLine());
+                    BMR = (447.6 + (9.2 * kilo)) + (3.2 * boy) - (4.3 * yaş);
+                   
+                    
+                    break;
+
+
+
+
+
+            }
+        hareket:
+            Console.Clear();
+            Console.WriteLine("Gün içinde ne kadar hareket ediyorsunuz?");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("[1]Hareketsiz (masa başı, hiç spor yok)");
+            Console.WriteLine("[2]Az aktif (haftada 1-3 gün hafif spor)");
+            Console.WriteLine("[3]Orta aktif (haftada 3-5 gün orta düzey spor)");
+            Console.WriteLine("[4]Çok aktif (haftada 6-7 gün ağır spor):");
+            Console.WriteLine("[5]Aşırı aktif (profesyonel sporcu, ağır işçi):");
+            hareket = int.Parse(Console.ReadLine());
+
+            switch (hareket)
+            {
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Lütfen geçerli bir sayı giriniz");
+                    goto hareket;
+
+                case 1:
+                    TDEE = (BMR * 1.2);
+                    break;
+                case 2:
+                    TDEE = (BMR * 1.375);
+                    break;
+                case 3:
+                    TDEE = (BMR * 1.55);
+                    break;
+                case 4:
+                    TDEE = (BMR * 1.725);
+                    break;
+                case 5:
+                    TDEE = (BMR * 1.9);
+                    break;
+
+            }
+        hedef:
+            Console.Clear();
+            Console.WriteLine("Hedefin nedir?");
+            Console.WriteLine("--------------");
+            Console.WriteLine("[1]Kilo vermek istiyorum");
+            Console.WriteLine("[2]Kilo almak istiyorum");
+            Console.WriteLine("[3]Kilomu korumak istiyorum");
+            seçim = double.Parse(Console.ReadLine());
+            switch (seçim)
+            {
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Lütfen geçerli bir sayı giriniz");
+                    goto hedef;
+                case 1:
+                    sonuç = (TDEE - 500);
+                    break;
+                case 2:
+                    sonuç = (TDEE + 500);
+                    break;
+                case 3:
+                    sonuç = (TDEE);
+                    break;
+
+            }
+            Console.WriteLine($"Günlük almanız gerek kalori {sonuç} Kcal'dır");
+        }
+    }
+}
---
+++ Günlük Kalori Hesaplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Günlük Kalori Hesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Günlük Kalori Hesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("11016c3c-b3f5-48d3-9dca-41ae2d4a69e1")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
